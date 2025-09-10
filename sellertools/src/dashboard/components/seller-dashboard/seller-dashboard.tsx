838b3b65f4251b06d2ccb755c111902b65360973
Behnam Tehrani <behnam777@gmail.com>
2025-09-10 09:03:39 -0400

    test5

--- sellertools/src/dashboard/components/seller-dashboard/seller-dashboard.tsx
+++ sellertools/src/dashboard/components/seller-dashboard/seller-dashboard.tsx
@@ -1,4 +1,3 @@
-import Card from '@commercetools-uikit/card';
 import {
   CartIcon,
   CoinsIcon,
@@ -13,55 +12,14 @@ import LoadingSpinner from '@commercetools-uikit/loading-spinner';
 import SelectField from '@commercetools-uikit/select-field';
 import Spacings from '@commercetools-uikit/spacings';
 import Text from '@commercetools-uikit/text';
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useMemo } from 'react';
 import { useIntl } from 'react-intl';
+import { CMS_DEPLOYED_URL_KEY } from '../../../constants';
 import { useBusinessUnitContext } from '../../contexts/business-unit-context';
+import { useExternalUrl } from '../../hooks/use-external-url';
+import { DashboardCard } from '../dashboard-card';
 import messages from './messages';
 import styles from './seller-dashboard.module.css';
-import { useExternalUrl } from '../../hooks/use-external-url';
-import { CMS_DEPLOYED_URL_KEY } from '../../../constants';
-
-type DashboardCardProps = {
-  title: string;
-  icon: React.ReactElement;
-  onClick: () => void;
-  checkVisibility?: () => Promise<boolean>;
-};
-
-const DashboardCard: React.FC<DashboardCardProps> = ({
-  title,
-  icon,
-  onClick,
-  checkVisibility,
-}) => {
-  const [isVisible, setIsVisible] = useState(checkVisibility ? false : true);
-
-  useEffect(() => {
-    if (checkVisibility) {
-      checkVisibility().then((visible) => {
-        console.log('visible', visible);
-        setIsVisible(visible);
-      });
-    }
-  }, [checkVisibility]);
-
-  if (!isVisible) {
-    return null;
-  }
-
-  return (
-    <Card className={styles.dashboardCard} onClick={onClick}>
-      <Spacings.Stack alignItems="center" scale="m">
-        <div className={styles.iconContainer}>
-          <div className={styles.iconWrapper}>
-            {React.cloneElement(icon, { size: 'big', color: 'surface' })}
-          </div>
-        </div>
-        <Text.Headline as="h3">{title}</Text.Headline>
-      </Spacings.Stack>
-    </Card>
-  );
-};
 
 type SellerDashboardProps = {
   onNavigate: (route: string) => void;
@@ -122,25 +80,25 @@ const SellerDashboard: React.FC<SellerDashboardProps> = ({ onNavigate }) => {
     {
       id: 'orders',
       title: intl.formatMessage(messages.viewOrders),
-      icon: <CartIcon />,
+      icon: <CartIcon size="big" color="surface" />,
       onClick: () => handleNavigate('/orders'),
     },
     {
       id: 'customers',
       title: intl.formatMessage(messages.manageCustomers),
-      icon: <UsersIcon />,
+      icon: <UsersIcon size="big" color="surface" />,
       onClick: () => handleNavigate('/customers'),
     },
     {
       id: 'products',
       title: intl.formatMessage(messages.selectProducts),
-      icon: <ListWithSearchIcon />,
+      icon: <ListWithSearchIcon size="big" color="surface" />,
       onClick: () => handleNavigate('/products'),
     },
     {
       id: 'prices',
       title: intl.formatMessage(messages.managePrices),
-      icon: <CoinsIcon />,
+      icon: <CoinsIcon size="big" color="surface" />,
       onClick: () => handleNavigate('/prices'),
     },
     // Temporarily hiding price list management card
@@ -153,26 +111,26 @@ const SellerDashboard: React.FC<SellerDashboardProps> = ({ onNavigate }) => {
     {
       id: 'promotions',
       title: intl.formatMessage(messages.managePromotions),
-      icon: <TagIcon />,
+      icon: <TagIcon size="big" color="surface" />,
       onClick: () => handleNavigate('/promotions'),
     },
     {
       id: 'content',
       title: intl.formatMessage(messages.manageContent),
-      icon: <FrontendStudioIcon />,
+      icon: <FrontendStudioIcon size="big" color="surface" />,
       onClick: () => handleNavigate('/content'),
       checkVisibility: checkHealthCms,
     },
     {
       id: 'reports',
       title: intl.formatMessage(messages.viewReports),
-      icon: <GraphIcon />,
+      icon: <GraphIcon size="big" color="surface" />,
       onClick: () => handleNavigate('/reports'),
     },
     {
       id: 'configuration',
       title: intl.formatMessage(messages.storeConfiguration),
-      icon: <GearIcon />,
+      icon: <GearIcon size="big" color="surface" />,
       onClick: () => handleNavigate('/configuration'),
     },
   ];
